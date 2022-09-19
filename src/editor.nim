2568123c391f621a98f83991dc6551fc5fcaec7a
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-09-19 17:17:49 +0300

    Add basic versions of the level editor and the game. Both of them take levels to edit/play as CLI arguments.

--- src/editor.nim
+++ src/editor.nim
@@ -1,3 +1,21 @@
+import strformat
 import cligen, frosty, nimraylib_now
 
-echo "test"
+proc editor(levels: seq[string]) =
+  var screenWidth = 1200'i32
+  var screenHeight = 750'i32
+  initWindow(screenWidth, screenHeight, "The Witness puzzle editor")
+  setTargetFPS(144)
+
+  while not windowShouldClose():
+    beginDrawing()
+    clearBackground(Darkgray)
+    if levels.len == 0:
+      drawText("Here the editor will ask for a level filename to edit", 300, 350, 20, Raywhite)
+    else:
+      drawText(fmt"""Here the editor will open the following levels in different tabs: {levels.join(", ")}""", 200, 350, 20, Raywhite)
+    endDrawing()
+
+  closeWindow()
+
+dispatch editor # Automatically generate a CLI that can be used to open one or more levels for editing
--- src/game.nim
+++ src/game.nim
@@ -1,3 +1,21 @@
+import strformat
 import cligen, frosty, nimraylib_now
 
-echo "test"
+proc game(level: seq[string]) =
+  var screenWidth = 1200'i32
+  var screenHeight = 750'i32
+  initWindow(screenWidth, screenHeight, "The Witness puzzles clone game")
+  setTargetFPS(144)
+
+  while not windowShouldClose():
+    beginDrawing()
+    clearBackground(Darkgray)
+    if level.len == 0:
+      drawText("Here the game will display a level select screen which shows the levels in ./levels", 200, 350, 20, Raywhite)
+    else:
+      drawText(fmt"Here the game will open the level with filename {level[0]}", 250, 350, 20, Raywhite)
+    endDrawing()
+
+  closeWindow()
+
+dispatch game # Automatically generate a CLI that can be used to choose a level to play
