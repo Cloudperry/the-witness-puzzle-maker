1c020e5d2136e8fe574c15d48f489ae675869e47
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-04 04:36:12 +0300

    Make editor and game draw the levels and handle loading them better

--- src/editor.nim
+++ src/editor.nim
@@ -1,6 +1,6 @@
 import strformat
 import cligen, nimraylib_now
-import levels
+import levels, levelGfx
 
 proc editor(levels: seq[string]) =
   # Init
@@ -9,11 +9,20 @@ proc editor(levels: seq[string]) =
     screenHeight = 750'i32
     levelName: string
     level: Level
+    drawOptions: DrawOptions 
+    levelScreenVertices: LevelScreenVertices
     editingLevel: bool
+  
+  proc loadLevel() =
+    level = loadLevelFromFile(levelName)
+    drawOptions = drawOptions((screenWidth.int, screenHeight.int))
+    levelScreenVertices = level.getScreenVertices(drawOptions)
+    editingLevel = true
+
   if levels.len > 0:
     levelName = levels[0]
-    editingLevel = true
-    level = loadLevelFromFile(levelName)
+    loadLevel()
+  setConfigFlags(MSAA_4X_HINT)
   initWindow(screenWidth, screenHeight, "The Witness puzzle editor")
   setTargetFPS(144)
 
@@ -24,17 +33,17 @@ proc editor(levels: seq[string]) =
       while key in 32 .. 125:
         levelName &= key.char
         key = getCharPressed()
-      if isKeyPressed(BACKSPACE) and levelName.len >= 1:
+      if isKeyPressed(BACKSPACE) and levelName.len > 0:
         levelName.setLen(levelName.len - 1)
-      elif isKeyPressed(ENTER):
-        editingLevel = true
-        level = loadLevelFromFile(levelName)
+      elif isKeyPressed(ENTER) and levelName.len > 0:
+        loadLevel()
 
     # Draw
     beginDrawing()
     clearBackground(Darkgray)
     if editingLevel:
       drawText(fmt"Loaded level {levelName}", 200, 350, 20, Raywhite)
+      level.draw(levelScreenVertices)
     else:
       drawText(fmt"Type level name to edit and press ENTER: " & levelName, 300, 350, 20, Raywhite)
     endDrawing()
--- src/game.nim
+++ src/game.nim
@@ -1,21 +1,53 @@
 import strformat
 import cligen, nimraylib_now
+import levels, levelGfx
 
-proc game(level: seq[string]) =
-  var screenWidth = 1200'i32
-  var screenHeight = 750'i32
-  initWindow(screenWidth, screenHeight, "The Witness puzzles clone game")
+proc editor(levels: seq[string]) =
+  # Init
+  var
+    screenWidth = 1200'i32
+    screenHeight = 750'i32
+    levelName: string
+    level: Level
+    drawOptions: DrawOptions 
+    levelScreenVertices: LevelScreenVertices
+    editingLevel: bool
+  
+  proc loadLevel() =
+    level = loadLevelFromFile(levelName)
+    drawOptions = drawOptions((screenWidth.int, screenHeight.int))
+    levelScreenVertices = level.getScreenVertices(drawOptions)
+    editingLevel = true
+
+  if levels.len > 0:
+    levelName = levels[0]
+    loadLevel()
+  setConfigFlags(MSAA_4X_HINT)
+  initWindow(screenWidth, screenHeight, "The Witness")
   setTargetFPS(144)
 
-  while not windowShouldClose():
+  while not windowShouldClose(): # Main loop
+    # Update
+    if not editingLevel:
+      var key = getCharPressed()
+      while key in 32 .. 125:
+        levelName &= key.char
+        key = getCharPressed()
+      if isKeyPressed(BACKSPACE) and levelName.len > 0:
+        levelName.setLen(levelName.len - 1)
+      elif isKeyPressed(ENTER) and levelName.len > 0:
+        loadLevel()
+
+    # Draw
     beginDrawing()
     clearBackground(Darkgray)
-    if level.len == 0:
-      drawText("Here the game will display a level select screen which shows the levels in ./levels", 200, 350, 20, Raywhite)
+    if editingLevel:
+      drawText(fmt"Loaded level {levelName}", 200, 350, 20, Raywhite)
+      level.draw(levelScreenVertices)
     else:
-      drawText(fmt"Here the game will open the level with filename {level[0]}", 250, 350, 20, Raywhite)
+      drawText(fmt"Type level name to play and press ENTER: " & levelName, 300, 350, 20, Raywhite)
     endDrawing()
 
   closeWindow()
 
-dispatch game # Automatically generate a CLI that can be used to choose a level to play
+dispatch editor # Automatically generate a CLI that can be used to open one or more levels for editing
