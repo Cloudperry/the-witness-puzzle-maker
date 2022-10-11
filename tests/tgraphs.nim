4d5639fbcf87a6c03496ededf7d414bc581e51b2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-11 18:13:17 +0300

    Use new contains function for checking if a point is part of the graph. Update comment.

--- tests/tgraphs.nim
+++ tests/tgraphs.nim
@@ -1,19 +1,19 @@
 import std/[tables, sets]
-import ../src/graphs
+import ../src/graphs, ../src/geometry
 
 var testGraph: PointGraph
 
 testGraph.addGridPoints((0.0, 0.0), (4.0, 4.0)) #Create a 5x5 grid of nodes from (0, 0) to (4, 4)
 for x, y in gridPoints((0.0, 0.0), (4.0, 4.0)):
-  assert (x, y) in testGraph.adjList #Check that each coordinate has the node
+  assert (x, y) in testGraph #Check that each coordinate has the node
 for point in gridPoints((0.0, 0.0), (4.0, 4.0)):
   for adjacentPoint in adjacentGridPoints(point):
-    if adjacentPoint in testGraph.adjList:
+    if adjacentPoint in testGraph:
       #Check that there are no connections in the graph
       assert testGraph.hasRoute(point, adjacentPoint) == false
 
 testGraph.connectGridPoints((0.0, 0.0), (4.0, 4.0))
-#Remove all connections to points (2, 2), (3, 2) and (2, 3)
+# Remove all outside connections to points (2, 2), (3, 2) and (2, 3)
 testGraph.removeEdge((2.0, 1.0), (2.0, 2.0))
 testGraph.removeEdge((1.0, 2.0), (2.0, 2.0))
 testGraph.removeEdge((3.0, 1.0), (3.0, 2.0))
