7555fb7575affda377d096400b0750479560f8b2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-11 18:15:14 +0300

    Add level editing functions

--- src/levels.nim
+++ src/levels.nim
@@ -1,6 +1,6 @@
-import std/[tables, sets]
+import std/[tables, sets, sequtils]
 import frosty/streams
-import graphs
+import graphs, geometry
 
 type
   Color* = tuple[r, g, b: int]
@@ -39,13 +39,44 @@ type
     bgColor*: Color
     lineColor*: Color
 
-# Soon I will create some level editing primitives in this module that make it easy to
-# create a gui level editor. And then I will use them to make an editor in editor.nim.
-# There is an example level in tests/tlevels.nim. That level should be rewritten as a 
-# test for the new level editing primitives once they are done.
+## Level editing primitives that are used by the level editor
+proc makeEmptyGrid*(l: var Level; topLeftCorner, botRightCorner: Point2D) =
+  for p1 in gridPoints(topLeftCorner, botRightCorner):
+    # Create lines around a maze cell
+    l.pointData[p1] = Empty
+    for p2 in [p1 + (1.0, 0.0), p1 + (0.0, 1.0)]:
+      if p2.x <= 4.0 and p2.y <= 4.0:
+        l.pointGraph.addEdgeAndMissingNodes(p1, p2)
+    # Create empty maze cell
+    if p1.x in 0.0 .. 3.0 and p1.y in 0.0 .. 3.0:
+      let cell = @[p1, p1 + (1.0, 0.0), p1 + (1.0, 1.0), p1 + (0.0, 1.0)]
+      l.cellGraph.addNode(cell)
+      l.cellData[cell] = MazeCell(kind: Empty)
+  # Connect all the adjacent cells together in the graph
+  for cell in l.cellGraph.nodes:
+    let cellToRight = cell.mapIt(it + (1.0, 0.0))
+    let cellBelow = cell.mapIt(it + (0.0, 1.0))
+    if cellToRight in l.cellGraph:
+      l.cellGraph.addEdge(cell, cellToRight)
+    if cellBelow in l.cellGraph:
+      l.cellGraph.addEdge(cell, cellBelow)
 
+proc setPointData*(l: var Level, pointData: Table[Point2D, PointKind]) = 
+  for point, kind in pointData:
+    if point in l.pointGraph:
+      l.pointData[point] = kind
+
+proc setCellData*(l: var Level, cellData: Table[seq[Point2D], MazeCell]) = 
+  for cell, data in cellData:
+    if cell in l.cellGraph:
+      l.cellData[cell] = data
+
+## Saving and loading levels is implemented below
 # Weird workaround used because of openFileStream errors. According to frosty sample code
 # I shouldn't need to import std/streams at all.
+#TODO: Maybe all the hash tables and sets in the level should be shrinked down to exactly the length
+# of their elements before saving it to a file. That way the level file will be much smaller and it
+# helps with iteration performance as well.
 import std/streams as s
 proc saveLevelToFile*(l: Level, filename: string) =
   var handle = openFileStream(filename, fmWrite)
