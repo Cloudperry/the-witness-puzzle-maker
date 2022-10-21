7c522ccb5e2f420ef51b7a29b2300152b3214e48
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-21 15:16:22 +0300

    Add operator for making an edge out of two points. Change findReachableNodes and hasRoute to use new dfs function.

--- src/graphs.nim
+++ src/graphs.nim
@@ -12,13 +12,7 @@ type
     adjList*: Table[N, HashSet[N]]
   Edge*[N] = tuple[node1, node2: N] ## An edge of a graph in the form (node1, node2)
 
-proc addNode*[T](g: var Graph, node: T) = g.adjList[node] = initHashSet[T](4)
-proc removeNode*[T](g: var Graph, node: T) =
-  var removedNodeNeighbors: HashSet[T]
-  if g.adjList.pop(node, removedNodeNeighbors):
-    for neighbor in removedNodeNeighbors: 
-      g.adjList[neighbor].excl node
-proc contains*[T](g: Graph, node: T): bool = node in g.adjList
+proc `<->`*[N](node1, node2: N): Edge[N] = (node1, node2)
 
 proc addEdge*[T](g: var Graph; node1, node2: T) =
   g.adjList[node1].incl node2
@@ -37,6 +31,14 @@ proc removeEdge*[T](g: var Graph; node1, node2: T) =
   g.adjList[node2].excl node1
 proc `$`*(e: Edge): string = fmt"({e.node1} <-> {e.node2})"
 
+proc addNode*[T](g: var Graph, node: T) = g.adjList[node] = initHashSet[T](4)
+proc removeNode*[T](g: var Graph, node: T) =
+  var removedNodeNeighbors: HashSet[T]
+  if g.adjList.pop(node, removedNodeNeighbors):
+    for neighbor in removedNodeNeighbors: 
+      g.adjList[neighbor].excl node
+proc contains*[T](g: Graph, node: T): bool = node in g.adjList
+
 iterator nodes*[T](g: Graph[T]): T = # NIMNOTE: Iterators work just like python generator functions
   for node in g.adjList.keys:
     yield node
@@ -51,28 +53,26 @@ proc getEdges*[T](g: Graph[T]): HashSet[Edge[T]] =
       if (node2, node1) notin result:
         result.incl (node1, node2)
 
-func findReachableNodes*[T](g: Graph, node: T): HashSet[T] =
+func dfs*[T](g: Graph, node: T, stopAt: HashSet[T]): HashSet[T] =
   # NIMNOTE: "Normal" functions in Nim are declared with proc and functions 
   # that don't have any side-effects are declared with func. These functions
   # can't modify any variables outside their scope and can't print for example.
-  var visited = initHashSet[T]()
-  # Inner function used so that the visited set can be initialized outside the dfs function
-  proc findReachableNodesInner[T](g: Graph, node: T) =
+  var visited: HashSet[T]
+  # Line below is a workaround for a Nim bug. dfsInner should get all variables that are from dfs'
+  # scope. Using generics somehow confuses the compiler and stopAt doesn't get passed through 
+  # without declaring it again in the body.
+  let stopAt = stopAt 
+  proc dfsInner[T](g: Graph, node: T) =
     visited.incl node
+    if node in stopAt: return
     for neighbor in g.adjList[node]:
       if neighbor notin visited:
-        g.findReachableNodesInner(neighbor)
-  findReachableNodesInner(g, node)
+        g.dfsInner(neighbor)
+  dfsInner(g, node)
   return visited
 
-func hasRoute*[T](g: Graph, currNode, goalNode: T): bool =
-  var visited = initHashSet[T]()
-  # Same as the previous function, but the dfs terminates when goal node is found
-  proc hasRouteInner[T](g: Graph, currNode, goalNode: T) =
-    visited.incl currNode
-    if currNode == goalNode: return
-    for neighbor in g.adjList[currNode]:
-      if neighbor notin visited:
-        g.hasRouteInner(neighbor, goalNode)
-  hasRouteInner(g, currNode, goalNode)
-  return goalNode in visited
+func findReachableNodes*[T](g: Graph, node: T): HashSet[T] = 
+  g.dfs(node, stopAt = initHashSet[T]())
+
+func hasRoute*[T](g: Graph, startNode, goalNode: T): bool =
+  return goalNode in g.dfs(startNode, stopAt = [goalNode].toHashSet)
