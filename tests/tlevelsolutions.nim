0fff42a8a25cf32cea8a3c1c2ed86586a64efefb
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-21 15:18:45 +0300

    Add new levels and solutions to test. Levels and solutions are now in a hash table and new ones can be added there.

--- tests/tlevelsolutions.nim
+++ tests/tlevelsolutions.nim
@@ -1,29 +1,68 @@
-import std/[tables, sets, lenientops, os]
-import ../src/levels, ../src/graphs, ../src/geometry
+import std/[strformat, tables, sets, lenientops, os]
+import ../src/[levels, geometry]
 
-let 
-  hexTutorial1 = loadLevelFromFile("levels/hexTutorial1.bin")
-  hexTutorial2 = loadLevelFromFile("levels/hexTutorial2.bin") #TODO: Add a few tests for this level (its a bit more complex)
+type 
+  LevelSolutions = object
+    level: Level
+    correctLines: seq[Line]
+    incorrectLines: seq[Line]
 
-const wrongSolutionsTut1 = [ 
-  # This line is wrong because it doesn't start from a start point
-  (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> (1.0, 2.0) -> (2.0, 2.0) -> 
-  (2.0, 1.0) -> (2.0, 0.0) -> (2.0, -0.25),
-  # This line is wrong because it doesn't go through any of the hexes
-  (0.0, 2.0) -> (1.0, 2.0) -> (1.0, 1.0) -> (2.0, 1.0) -> (2.0, 0.0) ->
-  (2.0, -0.25)
-]
-const correctSolutionsTut1 = [
-  (0.0, 2.0) -> (0.0, 1.0) -> (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> 
-  (1.0, 2.0) -> (2.0, 2.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (2.0, -0.25)
-]
+let levelsAndSolutions = {
+  "hexTutorial1": LevelSolutions(
+    level: loadLevelFromFile("levels/hexTutorial1.bin"),
+    correctLines: @[
+      (0.0, 2.0) -> (0.0, 1.0) -> (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> 
+      (1.0, 2.0) -> (2.0, 2.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (2.0, -0.25)
+    ],
+    incorrectLines: @[
+      # This line is wrong because it doesn't start from a start point
+      (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> (1.0, 2.0) -> (2.0, 2.0) -> 
+      (2.0, 1.0) -> (2.0, 0.0) -> (2.0, -0.25),
+      # This line is wrong because it doesn't go through any of the hexes
+      (0.0, 2.0) -> (1.0, 2.0) -> (1.0, 1.0) -> (2.0, 1.0) -> (2.0, 0.0) ->
+      (2.0, -0.25)
+    ]
+  ),
+  "hexTutorial2": LevelSolutions(
+    level: loadLevelFromFile("levels/hexTutorial2.bin") #TODO: Add a few tests for this level (its a bit more complex)
+  ),
+  "shipwreckTriangles": LevelSolutions(
+    level: loadLevelFromFile("levels/shipwreckTriangles.bin"),
+    correctLines: @[
+      (0.0, 3.0) -> (1.0, 3.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> (2.0, 2.0) -> 
+      (1.0, 2.0) -> (1.0, 1.0) -> (2.0, 1.0) -> (3.0, 1.0) -> (3.0, 0.0) -> (3.25, -0.25)
+    ],
+    incorrectLines: @[
+      (0.0, 3.0) -> (1.0, 3.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> 
+      (2.0, 2.0) -> (2.0, 1.0) -> (3.0, 1.0) -> (3.0, 0.0) -> (3.25, -0.25)
+    ]
+  ),
+  "treehouseBridge1Puzzle8": LevelSolutions(
+    level: loadLevelFromFile("levels/treehouseBridge1Puzzle8.bin"),
+    correctLines: @[
+      (1.5, 3.0) -> (1.0, 3.0) -> (0.0, 3.0) -> (0.0, 2.0) -> (1.0, 2.0) -> 
+      (2.0, 2.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> (3.0, 1.0) ->
+      (3.0, 0.0) -> (2.0, 0.0) -> (2.0, 1.0) -> (1.0, 1.0) -> (0.0, 1.0) -> 
+      (0.0, 0.0) -> (1.0, 0.0) -> (1.5, 0.0) -> (1.5, -0.25)
+    ]
+  ),
+  "quarryMetalBridgePanel2": LevelSolutions(
+    level: loadLevelFromFile("levels/quarryMetalBridgePanel2.bin"),
+  )
+}.toTable
 
-for line in wrongSolutionsTut1: 
-  # Check that the solution checker denies all the wrong solutions
-  assert not hexTutorial1.checkSolution(line)
-
-for line in correctSolutionsTut1:
+for name, level in levelsAndSolutions: 
   # Check that the solution checker accepts all the correct solutions
-  assert hexTutorial1.checkSolution(line)
+  for line in level.correctLines:
+    assert(
+      line.goesFromStartToEnd(level.level) and level.level.checkSolution(line),
+      fmt"Level {name} line {line} should be a correct solution"
+    ) 
+  # Check that the solution checker denies all the wrong solutions
+  for line in level.incorrectLines:
+    assert(
+      not (line.goesFromStartToEnd(level.level) and level.level.checkSolution(line)),
+      fmt"Level {name} line {line} should be an incorrect solution"
+    ) 
 
 echo "level solution algorithm (levels.nim) tests ran successfully."
