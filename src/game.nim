695e8eeff5b2cf643408184447de1df85947a51c
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-22 02:17:39 +0300

    Move code that is used for implementing the game to game.nim. Change line updating code.

--- src/game.nim
+++ src/game.nim
@@ -1,190 +1,146 @@
-import std/[strformat, strutils, options, tables, sets, monotimes, times, os]
-import cligen, nimraylib_now
-import levels, levelGfx, geometry, graphs
+import std/[math, sets, tables, sequtils, options, sugar]
+import geometry, levels, graphs
 
-when defined(windows):
-  {.passL: "-static".} # Use static linking on Windows
+type
+  PuzzleSym = enum
+    Hex, Square, Triangles, Star, Block, AntiBlock, Jack
 
-const 
-  fontSize = 30
-  textFlashDuration = 2000
-  moveRepeatDuration = 400
+  LineStatus* = enum
+    ## This is used when adding a new point to the player's line
+    None, OnlyGridNeighbors, HasDiagNeighbors, CorrectSolution, IncorrectSolution
 
-proc doTextEntry(text: var string): bool =
-  var key = getCharPressed()
-  while key in 32 .. 125:
-    text &= key.char
-    key = getCharPressed()
-  if isKeyPressed(BACKSPACE) and text.len > 0:
-    text.setLen(text.len - 1)
-  elif isKeyPressed(DELETE) and text.len > 0:
-    text.setLen(0)
-  elif isKeyPressed(ENTER) and text.len > 0:
-    return true
+  GameState* = object
+    level*: Level
+    line*: Line
+    status*: LineStatus
+    nxtMoves*: seq[Point2D]
 
-proc game(startingLine = "", levels: seq[string]) =
-  # Game state could be moved into an object. That would make for example
-  # unloading the level extremely simple.
-  # Init
-  var 
-    # The CLI generator I use didn't like vars in the proc parameters.
-    # Below is a workaround for that.
-    levels = levels
-    screenWidth = 1280
-    screenHeight = 720
-    selectedLevel: int
-    levelName: string
-    level: Level
-    drawOptions: DrawOptions 
-    drawableLevel: DrawableLevel
-    playingLevel: bool
-    currentPointStr: string
-    playerLine: Line
-    currentTime, prevTime: MonoTime
-    textFlashTimer, deltaTime: int64
-    lastPointAdd: PointAddResult
-  
-  proc unloadLevel() =
-    level = Level()
-    drawOptions = DrawOptions()
-    drawableLevel = DrawableLevel()
-    playingLevel = false
-    lastPointAdd = None
-    playerLine.setLen 0
+  MazeRoom = object
+    startCell: seq[Point2D]
+    unsolvedSyms: CountTable[PuzzleSym]
+    uncheckedSyms: CountTable[MazeCell]
 
-  proc loadLevel() =
-    level = loadLevelFromFile(levelName)
-    drawOptions = DrawOptions()
-    drawOptions.setPositionDefaults((screenWidth, screenHeight))
-    drawableLevel = level.getDrawableLevel(drawOptions)
-    playingLevel = true
+func goesFromStartToEnd*(line: Line, l: Level): bool = 
+  var lineHasRoute = true
+  for segment in line.segments:
+    if not l.pointGraph.hasRoute(segment.p1, segment.p2):
+      lineHasRoute = false
+  return l.pointData.getOrDefault(line[0]) == Start and 
+  l.pointData.getOrDefault(line[^1]) == End and lineHasRoute
 
-  proc addPoint(point: Option[Point2D]) =
-    if point.isSome:
-      let point = point.get
-      lastPointAdd = playerLine.checkAndAddPoint(point, level)
-      if lastPointAdd == Invalid:
-        textFlashTimer += deltaTime
-      currentPointStr.setLen 0
+func divideSymbolsToRooms*(level: Level, line: Line): seq[MazeRoom] =
+  ## Divides the level into rooms based on the player's line. Checks triangle symbols
+  ## at the same time, because all the relevant information is available here.
+  let lineSegments = line.toSetOfSegments()
+  var visited: HashSet[seq[Point2D]]
 
-  setConfigFlags(MSAA_4X_HINT)
-  setConfigFlags(FULLSCREEN_MODE)
-  initWindow(screenWidth, screenHeight, "The Witness")
-  let monitor = getCurrentMonitor()
-  screenWidth = getMonitorWidth(monitor)
-  screenHeight = getMonitorHeight(monitor)
-  let refreshRate = getMonitorRefreshRate(monitor)
-  setTargetFPS(refreshRate)
-  echo fmt"Set window to {screenWidth}x{screenHeight}@{refreshRate}hz"
+  func findSymsInRoom(node: seq[Point2D], room: var MazeRoom) =
+    visited.incl node
+    let nodeSegments = toSetOfSegments(node & node[0])
+    if node in level.cellData:
+      if level.cellData[node].kind == Triangles:
+        if (nodeSegments * lineSegments).len != level.cellData[node].count:
+          room.unsolvedSyms.inc Triangles # Add one unsolved triangle symbol to room
+      else: 
+        # This symbol will be checked after the room division is done
+        room.uncheckedSyms.inc level.cellData[node] 
 
-  if levels.len > 0:
-    levelName = levels[0]
-    if levels.len == 1:
-      loadLevel()
+    for neighbor in level.cellGraph.adjList[node]:
+      if neighbor notin visited:
+        let neighborSegments = toSetOfSegments(neighbor & neighbor[0])
+        # Two non-overlapping polygons have 0 sides in common with the line if they are in the same room
+        if len(nodeSegments * neighborSegments * lineSegments) == 0:
+          findSymsInRoom(neighbor, room)
+
+  for cell in level.cellGraph.nodes:
+    if cell notin visited:
+      var currRoom = MazeRoom(startCell: cell)
+      findSymsInRoom(cell, currRoom)
+      result.add currRoom
+
+proc findUnsolvedHexes(level: Level, line: Line, r: var MazeRoom) =
+  ## Finds unsolved hexagons in a given room with a DFS. Room is given as 
+  ## a cell by the room division of the previous function.
+  var visited: HashSet[Point2D]
+  let linePoints = line.toHashSet()
+
+  proc findHexesInRoom(point: Point2D, r: var MazeRoom) =
+    visited.incl point
+    if level.pointData.getOrDefault(point) == Hex:
+      r.unsolvedSyms.inc Hex
+    for neighbor in level.pointGraph.adjList[point]:
+      if neighbor notin visited and neighbor notin linePoints:
+        findHexesInRoom(neighbor, r)
+
+  for point in r.startCell:
+    if point notin linePoints:
+      findHexesInRoom(point, r)
+      break
+
+func checkSolution*(level: Level, line: Line): bool =
+  var rooms = level.divideSymbolsToRooms(line)
+  for room in rooms.mitems:
+    level.findUnsolvedHexes(line, room)
+    var squares, stars: CountTable[Color]
+    for symbol, count in room.uncheckedSyms:
+      if symbol.kind == Square:
+        squares.inc(symbol.color, count) 
+      elif symbol.kind == Star:
+        stars.inc(symbol.color, count) 
+    # Find unsolved squares
+    if squares.len > 1:
+      squares.sort()
+      room.unsolvedSyms[Square] = squares.values.toSeq()[1..^1].sum()
+    # Find unsolved stars
+    for color, count in stars:
+      let squareCount = squares[color]
+      if squareCount notin {0, 1} or (count + squareCount) mod 2 != 0:
+        room.unsolvedSyms[Star] = count
+    # Fix unsolved symbols if there are jacks
+    let unsolvedSymsAfterJacks = room.unsolvedSyms.values.toSeq().sum() - room.uncheckedSyms[MazeCell(kind: Jack)] 
+    if unsolvedSymsAfterJacks != 0:
+      # Not all unsolved symbols could be fixed by jacks or not all jacks were used
+      when defined(printReasonNotSolved):
+        debugEcho fmt"room {room.startCell} unsolved symbols {room.unsolvedSyms}, jacks {room.uncheckedSyms[MazeCell(kind: Jack)]}"
+      return false
+  return true
+  #TODO: Implement blocks (polyominos)
+
+proc init*(game: var GameState, levelName: string) =
+  game = GameState()
+  game.level = loadLevelFromFile(levelName)
+  for point, kind in game.level.pointData:
+    if kind == Start:
+      game.nxtMoves.add point
+
+proc addPointAndCheckResult*(game: var GameState, point: Point2D) =
+  if game.line.len >= 2 and point == game.line[^2]:
+    game.line.del game.line.high
   else:
-    for filename in walkPattern("levels/*.bin"):
-      levels.add filename
-  if startingLine.len > 0:
-    for pointStr in startingLine.split(','):
-      addPoint pointStr.parsePoint()
+    game.line &= point
 
-  while not windowShouldClose(): # Main loop
-    prevTime = currentTime
-    currentTime = getMonoTime()
-    deltaTime = inMilliseconds(currentTime - prevTime)
-    # Make timer handling library code if any more timers are added
-    if textFlashTimer >= textFlashDuration:
-      textFlashTimer = 0
-    elif textFlashTimer > 0:
-      textFlashTimer += deltaTime
-    # Update
-    if not playingLevel:
-      if levels.len == 0:
-        if doTextEntry(levelName):
-          loadLevel()
-      else:
-        levelName = levels[selectedLevel]
-        if isKeyPressed(KeyboardKey.LEFT):
-          if selectedLevel > 0:
-            dec selectedLevel
-        elif isKeyPressed(KeyboardKey.RIGHT):
-          if selectedLevel < levels.high:
-            inc selectedLevel
-        elif isKeyPressed(ENTER):
-          loadLevel()
+  if game.line.goesFromStartToEnd(game.level):
+    if game.level.checkSolution(game.line):
+      game.status = CorrectSolution
     else:
-      if lastPointAdd notin {CorrectSolution, IncorrectSolution}:
-        if isKeyDown(C):
-          playerLine.setLen(0)
-        elif isKeyPressed(KeyboardKey.LEFT):
-          addPoint playerLine.pointInDirection((-1.0, 0.0), level)
-        elif isKeyPressed(KeyboardKey.RIGHT):
-          addPoint playerLine.pointInDirection((1.0, 0.0), level)
-        elif isKeyPressed(DOWN):
-          addPoint playerLine.pointInDirection((0.0, 1.0), level)
-        elif isKeyPressed(UP):
-          addPoint playerLine.pointInDirection((0.0, -1.0), level)
-        elif doTextEntry(currentPointStr):
-          addPoint currentPointStr.parsePoint()
+      game.status = IncorrectSolution
+  else:
+    game.status = OnlyGridNeighbors # The added point only has horizontal and vertical neighbors
+    var diagonals: seq[Point2D]
+    for neighbor in game.level.pointGraph.adjList[point]:
+      if not isDiagonal(neighbor - point):
+        game.nxtMoves.add neighbor
       else:
-        if isKeyPressed(R):
-          lastPointAdd = None
-          playerLine.setLen(0)
-        elif isKeyPressed(S):
-          unloadLevel()
-        elif isKeyPressed(P):
-          echo playerLine
+        diagonals.add neighbor
+        game.status = HasDiagNeighbors # The added point only has horizontal and vertical neighbors
 
-    # Draw
-    beginDrawing()
+    if game.status == HasDiagNeighbors: game.nxtMoves = diagonals
 
-    clearBackground(Darkgray)
-    if not playingLevel:
-      if levels.len > 0:
-        let padding = screenWidth div 80
-        var currWidth = padding
-        for i, level in levels:
-          var text = fmt"""{levels[i].replace("levels/")}"""
-          if i == selectedLevel: 
-            text = '[' & text & ']'
-          drawText(text.cstring, currWidth, screenHeight div 2, fontSize, Raywhite) 
-          currWidth += measureText(text.cstring, fontSize) + padding
-      else:
-        let text = fmt"Type level path to play and press ENTER: {levelName}" 
-        let textWidth = measureText(text.cstring, fontSize)
-        # Text is not vertically centered, because using measureTextEx was giving me segfaults
-        drawText(text.cstring, screenWidth div 2 - textWidth div 2, 
-                 screenHeight div 2, fontSize, Raywhite) 
-    else:
-      # Draw text
-      let textOffsetY = drawableLevel.mazeDistToScreen(
-        dist(level.topLeftCorner.y, drawableLevel.topLeft.y) / 2, drawOptions
-      )
-      let textTop = fmt"playing {levelName}"
-      let textTopWidth = measureText(textTop.cstring, fontSize)
-      drawText(textTop.cstring, screenWidth div 2 - textTopWidth div 2, textOffsetY, fontSize, Raywhite)
-      var textBot: string
-      if lastPointAdd in {None, Valid, Invalid}:
-        textBot = fmt"Enter a new point (or press C to clear line): {currentPointStr}"
-        if textFlashTimer > 0:
-          textBot = "Invalid point."
-          if playerLine.len == 0:
-            textBot &= fmt" The first point of a line has to be one of the starting points (big circles)."
-          else:
-            textBot &= fmt" The point needs to be next to your previous point and it can't cross your line."
-      elif lastPointAdd == CorrectSolution:
-        textBot = "You have solved the level. Press esc to quit, s to select another level or r to restart the level."
-      elif lastPointAdd == IncorrectSolution:
-        textBot = "Your line goes from start to end, but the solution is incorrect. Press esc to quit, s to select another level or r to restart the level."
-      let textBotWidth = measureText(textBot.cstring, fontSize)
-      drawText(textBot.cstring, screenWidth div 2 - textBotWidth div 2, screenHeight - textOffsetY, fontSize, Raywhite)
-      # Draw level
-      level.draw(drawableLevel.gfxData, drawOptions)
-      if playerLine.len > 0:
-        level.drawPlayerLine(playerLine, drawableLevel, drawOptions)
-
-    endDrawing()
-
-  closeWindow()
-
-dispatch game # Automatically generate a CLI that can be used to open a level to play
+proc neighborInDirection*(game: var GameState, direction: Point2D): Option[Point2D] =
+  if game.line.len > 0:
+    let currPoint = game.line[^1]
+    let neighbors = collect: 
+      for neighbor in game.level.pointGraph.adjList[currPoint]:
+        {toUnitVec(neighbor - currPoint): neighbor}
+    if direction in neighbors:
+      result = some(neighbors[direction])
--- src/levels.nim
+++ src/levels.nim
@@ -8,8 +8,6 @@ type
     Empty, Start, End, Hex
   CellKind* = enum ## All the puzzle symbols that can be attached to a cell
     Empty, Square, Triangles, Star, Block, AntiBlock, Jack
-  PuzzleSym = enum
-    Hex, Square, Triangles, Star, Block, AntiBlock, Jack
   MazeCell* = object
     # NIMNOTE: Here kind is one field in the object, but the other fields the
     # object has depends on the value of kind. For example squares have a color
@@ -24,10 +22,6 @@ type
     of Jack, Empty:                  ## (0, 0) is the top left corner of the shape.
       discard
 
-  PointAddResult* = enum
-    ## This is used when adding a new point to the player's line
-    None, Invalid, Valid, CorrectSolution, IncorrectSolution
-    
   Level* = object
     # The below field could be used for arbitrary maze shapes
     # borderVertices*: seq[Point2D]
@@ -43,27 +37,23 @@ type
     fgColor*: Color
     bgColor*: Color
     lineColor*: Color
-
-  MazeRoom = object
-    startCell: seq[Point2D]
-    unsolvedSyms: CountTable[PuzzleSym]
-    uncheckedSyms: CountTable[MazeCell]
   
 func `==`*(c1, c2: MazeCell): bool =
   ## This comparison is needed for comparing level objects and when hashing cells
   if c1.kind == c2.kind:
-    if c1.kind in {Square, Star}:
+    if c1.kind in {CellKind.Square, Star}:
       return c1.color == c2.color
     elif c1.kind == Triangles:
       return c1.count == c2.count
-    elif c1.kind in {Block, AntiBlock}:
+    elif c1.kind in {CellKind.Block, AntiBlock}:
       return c1.shape == c2.shape
     elif c1.kind in {CellKind.Empty, Jack}:
       return true
 
 # --------------------------------Level creation--------------------------------
 # Error handling is important to have in the level creation functions, because
-# creating a level that uses the format in unintentional ways leads to weird bugs. 
+# creating a level that uses the format in unintentional ways leads to weird 
+# bugs in the level solution algorithm.
 func cellFromCornerAndDirection*(corner, direction: Point2D): seq[Point2D] = 
   result = @[
     corner, corner + (direction.x, 0.0), 
@@ -113,7 +103,7 @@ proc setCellData*(l: var Level, cellData: Table[MazeCell, seq[seq[Point2D]]]) =
       else:
         l.cellData[cell] = data
 
-proc addConnectedPoint*(l: var Level, newPoint: Point2D, kind = Empty,
+proc addConnectedPoint*(l: var Level, newPoint: Point2D, kind = PointKind.Empty,
                         connTo: varargs[Point2D]) =
   if newPoint in l.pointGraph:
     raise newException(ValueError, fmt"Point {newPoint} already exists")
@@ -146,7 +136,7 @@ proc removeEdges*(l: var Level, edges: varargs[Edge[Point2D]]) =
         fmt"Removing edge {edge} would leave a point without connections"
       )
 
-proc addPointBetween*(l: var Level; p1, p2: Point2D, kind = Empty) =
+proc addPointBetween*(l: var Level; p1, p2: Point2D, kind = PointKind.Empty) =
   let newPoint = midpoint(p1, p2)
   l.addConnectedPoint(newPoint, kind, p1, p2)
   l.pointGraph.removeEdge(p1, p2)
@@ -167,125 +157,3 @@ proc loadLevelFromFile*(filename: string): Level =
   var handle = openFileStream(filename, fmRead)
   thaw(handle, result)
   close handle
-
-# --------------------------------Game mechanics--------------------------------
-func goesFromStartToEnd*(line: Line, l: Level): bool = 
-  var lineHasRoute = true
-  for segment in line.segments:
-    if not l.pointGraph.hasRoute(segment.p1, segment.p2):
-      lineHasRoute = false
-  return l.pointData.getOrDefault(line[0]) == Start and 
-  l.pointData.getOrDefault(line[^1]) == End and lineHasRoute
-
-func divideSymbolsToRooms*(level: Level, line: Line): seq[MazeRoom] =
-  ## Divides the level into rooms based on the player's line. Checks triangle symbols
-  ## at the same time, because all the relevant information is available here.
-  let lineSegments = line.toSetOfSegments()
-  var visited: HashSet[seq[Point2D]]
-
-  func findSymsInRoom(node: seq[Point2D], room: var MazeRoom) =
-    visited.incl node
-    let nodeSegments = toSetOfSegments(node & node[0])
-    if node in level.cellData:
-      if level.cellData[node].kind == Triangles:
-        if (nodeSegments * lineSegments).len != level.cellData[node].count:
-          room.unsolvedSyms.inc Triangles # Add one unsolved triangle symbol to room
-      else: 
-        # This symbol will be checked after the room division is done
-        room.uncheckedSyms.inc level.cellData[node] 
-
-    for neighbor in level.cellGraph.adjList[node]:
-      if neighbor notin visited:
-        let neighborSegments = toSetOfSegments(neighbor & neighbor[0])
-        # Two non-overlapping polygons have 0 sides in common with the line if they are in the same room
-        if len(nodeSegments * neighborSegments * lineSegments) == 0:
-          findSymsInRoom(neighbor, room)
-
-  for cell in level.cellGraph.nodes:
-    if cell notin visited:
-      var currRoom = MazeRoom(startCell: cell)
-      findSymsInRoom(cell, currRoom)
-      result.add currRoom
-
-proc findUnsolvedHexes(level: Level, line: Line, r: var MazeRoom) =
-  ## Finds unsolved hexagons in a given room with a DFS. Room is given as 
-  ## a cell by the room division of the previous function.
-  var visited: HashSet[Point2D]
-  let linePoints = line.toHashSet()
-
-  proc findHexesInRoom(point: Point2D, r: var MazeRoom) =
-    visited.incl point
-    if level.pointData.getOrDefault(point) == Hex:
-      r.unsolvedSyms.inc Hex
-    for neighbor in level.pointGraph.adjList[point]:
-      if neighbor notin visited and neighbor notin linePoints:
-        findHexesInRoom(neighbor, r)
-
-  for point in r.startCell:
-    if point notin linePoints:
-      findHexesInRoom(point, r)
-      break
-
-func checkSolution*(level: Level, line: Line): bool =
-  var rooms = level.divideSymbolsToRooms(line)
-  for room in rooms.mitems:
-    level.findUnsolvedHexes(line, room)
-    var squares, stars: CountTable[Color]
-    for symbol, count in room.uncheckedSyms:
-      if symbol.kind == Square:
-        squares.inc(symbol.color, count) 
-      elif symbol.kind == Star:
-        stars.inc(symbol.color, count) 
-    # Find unsolved squares
-    if squares.len > 1:
-      squares.sort()
-      room.unsolvedSyms[Square] = squares.values.toSeq()[1..^1].sum()
-    # Find unsolved stars
-    for color, count in stars:
-      let squareCount = squares[color]
-      if squareCount notin {0, 1} or (count + squareCount) mod 2 != 0:
-        room.unsolvedSyms[Star] = count
-    # Fix unsolved symbols if there are jacks
-    let unsolvedSymsAfterJacks = room.unsolvedSyms.values.toSeq().sum() - room.uncheckedSyms[MazeCell(kind: Jack)] 
-    if unsolvedSymsAfterJacks != 0:
-      # Not all unsolved symbols could be fixed by jacks or not all jacks were used
-      when defined(printReasonNotSolved):
-        debugEcho fmt"room {room.startCell} unsolved symbols {room.unsolvedSyms}, jacks {room.uncheckedSyms[MazeCell(kind: Jack)]}"
-      return false
-  return true
-  #TODO: Implement blocks (polyominos)
-
-proc checkAndAddPoint*(line: var Line, point: Point2D, 
-                      level: Level): PointAddResult =
-  if line.len == 0:
-    if level.pointData.getOrDefault(point) != Start:
-      return Invalid
-    else:
-      line &= point
-      return Valid
-  else:
-    let eraseLast = line.len >= 2 and point == line[^2]
-    if point notin level.pointGraph.adjList[line[^1]] or 
-    (not eraseLast and point in line):
-      return Invalid
-    elif eraseLast:
-      line.del line.high
-    else:
-      line &= point
-      if line.goesFromStartToEnd(level):
-        if level.checkSolution(line):
-          return CorrectSolution
-        else:
-          return IncorrectSolution
-      return Valid
-
-func pointInDirection*(line: Line, direction: Point2D, level: Level): Option[Point2D] =
-  if line.len > 0:
-    let currPoint = line[^1]
-    proc toDirectionUnitVec(point: Vec2): Point2D =
-      toUnitVec(point - currPoint)
-    let neighbors = collect: 
-      for neighbor in level.pointGraph.adjList[currPoint]:
-        {neighbor.toDirectionUnitVec: neighbor}
-    if direction in neighbors:
-      result = some(neighbors[direction])
