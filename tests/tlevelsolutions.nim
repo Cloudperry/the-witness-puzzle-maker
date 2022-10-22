227955b01556821e5a70b9932f4278ad0f1502f9
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-10-22 22:29:25 +0300

    Add tests for all playable levels. Change the tests to use new game.nim functions.

--- tests/tlevelsolutions.nim
+++ tests/tlevelsolutions.nim
@@ -2,14 +2,14 @@ import std/[strformat, tables, sets, lenientops, os]
 import ../src/[levels, game, geometry]
 
 type 
-  LevelSolutions = object
-    level: Level
+  LevelSolutionsTest = object
+    levelName: string
     correctLines: seq[Line]
     incorrectLines: seq[Line]
 
-let levelsAndSolutions = {
-  "hexTutorial1": LevelSolutions(
-    level: loadLevelFromFile("levels/hexTutorial1.bin"),
+let tests = [
+  LevelSolutionsTest(
+    levelName: "hexTutorial1",
     correctLines: @[
       (0.0, 2.0) -> (0.0, 1.0) -> (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> 
       (1.0, 2.0) -> (2.0, 2.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (2.0, -0.25)
@@ -23,8 +23,8 @@ let levelsAndSolutions = {
       (2.0, -0.25)
     ]
   ),
-  "hexTutorial2": LevelSolutions(
-    level: loadLevelFromFile("levels/hexTutorial2.bin"),
+  LevelSolutionsTest(
+    levelName: "hexTutorial2",
     correctLines: @[
       (0.0, 3.0) -> (0.0, 2.0) -> (1.0, 2.0) -> (1.0, 3.0) -> (2.0, 3.0) -> 
       (3.0, 3.0) -> (3.0, 2.0) -> (2.0, 2.0) -> (2.0, 1.0) -> (1.0, 1.0) -> 
@@ -40,8 +40,8 @@ let levelsAndSolutions = {
       (3.0, 0.0) -> (3.0, -0.25)
     ]
   ),
-  "shipwreckTriangles": LevelSolutions(
-    level: loadLevelFromFile("levels/shipwreckTriangles.bin"),
+  LevelSolutionsTest(
+    levelName: "shipwreckTriangles",
     correctLines: @[
       (0.0, 3.0) -> (1.0, 3.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> (2.0, 2.0) -> 
       (1.0, 2.0) -> (1.0, 1.0) -> (2.0, 1.0) -> (3.0, 1.0) -> (3.0, 0.0) -> (3.25, -0.25)
@@ -51,32 +51,76 @@ let levelsAndSolutions = {
       (2.0, 2.0) -> (2.0, 1.0) -> (3.0, 1.0) -> (3.0, 0.0) -> (3.25, -0.25)
     ]
   ),
-  "treehouseBridge1Puzzle8": LevelSolutions(
-    level: loadLevelFromFile("levels/treehouseBridge1Puzzle8.bin"),
+  LevelSolutionsTest(
+    levelName: "treehouseBridge1Puzzle8",
     correctLines: @[
       (1.5, 3.0) -> (1.0, 3.0) -> (0.0, 3.0) -> (0.0, 2.0) -> (1.0, 2.0) -> 
       (2.0, 2.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> (3.0, 1.0) ->
       (3.0, 0.0) -> (2.0, 0.0) -> (2.0, 1.0) -> (1.0, 1.0) -> (0.0, 1.0) -> 
       (0.0, 0.0) -> (1.0, 0.0) -> (1.5, 0.0) -> (1.5, -0.25)
+    ],
+    incorrectLines: @[
+      (1.5, 3.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) -> (2.0, 2.0) ->
+      (1.0, 2.0) -> (1.0, 3.0) -> (0.0, 3.0) -> (0.0, 2.0) -> (0.0, 1.0) ->
+      (0.0, 0.0) -> (1.0, 0.0) -> (1.0, 1.0) -> (2.0, 1.0) -> (2.0, 0.0) ->
+      (1.5, 0.0) -> (1.5, -0.25)
     ]
   ),
-  "quarryMetalBridgePanel2": LevelSolutions(
-    level: loadLevelFromFile("levels/quarryMetalBridgePanel2.bin"),
+  LevelSolutionsTest(
+    levelName: "entryAreaDoor",
+    correctLines: @[
+      (6.0, 4.0) -> (6.5, 4.0) -> (7.0, 4.0) -> (7.0, 3.5) -> (7.0, 3.0) -> 
+      (7.0, 2.0) -> (7.0, 1.0) -> (7.0, 0.5) -> (7.0, 0.0) -> (6.5, 0.0) ->
+      (6.0, 0.0) -> (6.0, 1.0) -> (6.0, 2.0) -> (5.0, 2.0) -> (4.0, 2.0) ->
+      (3.0, 2.0) -> (2.0, 2.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (1.0, 0.0) ->
+      (1.0, 1.0) -> (0.0, 1.0) -> (0.0, 2.0) -> (0.0, 3.0) -> (0.0, 4.0) ->
+      (1.0, 4.0) -> (2.0, 4.0) -> (3.0, 4.0) -> (3.0, 5.0) -> (2.0, 5.0) ->
+      (1.5, 5.0) -> (1.0, 5.0) -> (0.0, 5.0) -> (0.0, 6.0) -> (0.0, 6.5) ->
+      (0.0, 7.0) -> (0.5, 7.0) -> (1.0, 7.0) -> (2.0, 7.0) -> (3.0, 7.0) ->
+      (4.0, 7.0) -> (5.0, 7.0) -> (5.5, 7.0) -> (6.0, 7.0) -> (6.0, 6.0) ->
+      (6.0, 5.0) -> (7.0, 5.0) -> (7.0, 6.0) -> (7.0, 7.0) -> (7.25, 7.0)
+    ],
+    incorrectLines: @[
+      (6.0, 4.0) -> (6.5, 4.0) -> (7.0, 4.0) -> (7.0, 3.5) -> (7.0, 3.0) ->
+      (7.0, 2.0) -> (7.0, 1.0) -> (7.0, 0.5) -> (7.0, 0.0) -> (6.5, 0.0) ->
+      (6.0, 0.0) -> (6.0, 1.0) -> (5.0, 1.0) -> (5.0, 2.0) -> (4.0, 2.0) ->
+      (3.0, 2.0) -> (3.0, 1.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (1.0, 0.0) ->
+      (1.0, 1.0) -> (0.0, 1.0) -> (0.0, 2.0) -> (0.0, 3.0) -> (0.0, 4.0) ->
+      (1.0, 4.0) -> (2.0, 4.0) -> (3.0, 4.0) -> (3.0, 5.0) -> (2.0, 5.0) ->
+      (1.5, 5.0) -> (1.0, 5.0) -> (0.0, 5.0) -> (0.0, 6.0) -> (0.0, 6.5) ->
+      (0.0, 7.0) -> (0.5, 7.0) -> (1.0, 7.0) -> (2.0, 7.0) -> (3.0, 7.0) ->
+      (4.0, 7.0) -> (5.0, 7.0) -> (5.5, 7.0) -> (6.0, 7.0) -> (6.0, 6.0) ->
+      (7.0, 6.0) -> (7.0, 7.0) -> (7.25, 7.0)
+    ]
+  ),
+  LevelSolutionsTest(
+    levelName: "quarryMetalBridgePanel2",
+    correctLines: @[
+      (0.0, 3.0) -> (1.0, 3.0) -> (2.0, 3.0) -> (3.0, 3.0) -> (3.0, 2.0) ->
+      (3.0, 1.0) -> (2.0, 1.0) -> (2.0, 0.0) -> (3.0, 0.0) -> (3.25, -0.25)
+    ],
+    incorrectLines: @[
+      (0.0, 3.0) -> (1.0, 3.0) -> (2.0, 3.0) -> (2.0, 2.0) -> (2.0, 1.0) ->
+      (2.0, 0.0) -> (3.0, 0.0) -> (3.25, -0.25)]
   )
-}.toTable
+]
 
-for name, level in levelsAndSolutions: 
+for test in tests: 
   # Check that the solution checker accepts all the correct solutions
-  for line in level.correctLines:
+  var game: GameState
+  game.init(fmt"levels/{test.levelName}.bin")
+  for line in test.correctLines:
+    game.setLine line
     assert(
-      line.goesFromStartToEnd(level.level) and level.level.checkSolution(line),
-      fmt"Level {name} line {line} should be a correct solution"
+      line.goesFromStartToEnd(game.level) and game.checkSolution(),
+      fmt"Level {test.levelName} line {line} should be a correct solution"
     ) 
   # Check that the solution checker denies all the wrong solutions
-  for line in level.incorrectLines:
+  for line in test.incorrectLines:
+    game.setLine line
     assert(
-      not (line.goesFromStartToEnd(level.level) and level.level.checkSolution(line)),
-      fmt"Level {name} line {line} should be an incorrect solution"
+      not (line.goesFromStartToEnd(game.level) and game.checkSolution()),
+      fmt"Level {test.levelName} line {line} should be an incorrect solution"
     ) 
 
 echo "level solution algorithm (game.nim) tests ran successfully."
